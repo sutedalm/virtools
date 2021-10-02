25293ac3d8dc9a6c128115fa0fb07b6fe79ed43b
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-10-02 12:35:50 +0200

    clean up autogenerated dummy files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  window.handsfree.enablePlugins('browser')
+  window.handsfree.enablePlugins("browser");
 
-  function startHandsfree () {window.handsfree.start()}
-  function stopHandsfree () {window.handsfree.stop()}
+  function startHandsfree() {
+    window.handsfree.start();
+  }
+  function stopHandsfree() {
+    window.handsfree.stop();
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         {/* These classes are included by handsfree.css */}
         <p>
-          <button className="handsfree-show-when-stopped handsfree-hide-when-loading" onClick={startHandsfree}>Start handsfree</button>
+          <button
+            className="handsfree-show-when-stopped handsfree-hide-when-loading"
+            onClick={startHandsfree}
+          >
+            Start handsfree
+          </button>
           <button className="handsfree-show-when-loading">...loading...</button>
-          <button className="handsfree-show-when-started" onClick={stopHandsfree}>Stop handsfree</button>
+          <button
+            className="handsfree-show-when-started"
+            onClick={stopHandsfree}
+          >
+            Stop handsfree
+          </button>
         </p>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import Handsfree from 'handsfree'
-import 'handsfree/build/lib/assets/handsfree.css'
+import Handsfree from "handsfree";
+import "handsfree/build/lib/assets/handsfree.css";
 
 window.handsfree = new Handsfree({
   handpose: true,
-  showDebug: true});
+  showDebug: true,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
