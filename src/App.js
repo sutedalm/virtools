339dab3926175684f62932b16d1514f7654309e3
Maria Pospelova <mariyapospelova@gmail.com>
2021-10-02 09:38:31 +0200

    Adding handsfree

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Handsfree from 'handsfree'
+import 'handsfree/build/lib/assets/handsfree.css'
+
 function App() {
+  const handsfree = new Handsfree({handpose: true})
+  handsfree.enablePlugins('browser')
+
+  function startHandsfree () {handsfree.start()}
+  function stopHandsfree () {handsfree.stop()}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* These classes are included by handsfree.css */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button className="handsfree-show-when-stopped handsfree-hide-when-loading" onClick={startHandsfree}>Start handsfree</button>
+          <button className="handsfree-show-when-loading">...loading...</button>
+          <button className="handsfree-show-when-started" onClick={stopHandsfree}>Stop handsfree</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
