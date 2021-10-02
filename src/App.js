6eb97c837202601926606fc9f1530b246fa77194
Maria Pospelova <mariyapospelova@gmail.com>
2021-10-02 12:56:55 +0200

    Trying to make a cube follow the index finger

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { BoxBufferGeometry, Mesh, MeshBasicMaterial } from "three";
+import { useEffect, useRef } from "react";
+
 function App() {
   window.handsfree.enablePlugins("browser");
 
@@ -8,10 +11,42 @@ function App() {
     window.handsfree.stop();
   }
 
+  console.log("handsfree", window.handsfree);
+
   const handPose = window.handsfree?.model?.handpose;
   const threeScene = handPose?.three?.scene;
-  console.log(handPose);
-  console.log(threeScene);
+  const indexFinger = handPose?.data?.landmarks?.[8];
+
+  console.log("handpose", handPose);
+
+  const cubeRef = useRef();
+  useEffect(() => {
+    const geometry = new BoxBufferGeometry(60, 60, 60);
+    const material = new MeshBasicMaterial();
+    cubeRef.current = new Mesh(geometry, material);
+  }, []);
+
+  useEffect(() => {
+    if (indexFinger) {
+      console.log("Centered Palm object", indexFinger);
+      cubeRef.current.position.set(
+        indexFinger[0],
+        indexFinger[1],
+        indexFinger[3]
+      );
+
+      console.log("Cube position", cubeRef.current.position);
+      console.log("Cube", cubeRef.current);
+      console.log("Index finger position", indexFinger);
+    }
+  }, [JSON.stringify(indexFinger)]);
+
+  useEffect(() => {
+    if (threeScene && cubeRef.current) {
+      console.log("ThreeScene", threeScene);
+      threeScene.add(cubeRef.current);
+    }
+  }, [threeScene, cubeRef.current]);
 
   return (
     <div className="App">
